3792b88cf22a4cc4aa004ea6d3c4df1d8419cb60
SaaSha <saasha.pina@gmail.com>
2018-04-20 13:29:34 -0400

    SaaSha Pina | W.I.P

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -5,6 +5,16 @@ const createUser = (req, res) => {
   const { username, password } = req.body;
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+  const user = new User({username, password});
+  user
+    .save()
+    .then(user => {
+      res.status(200).json({success: "User has been saved!", user});
+    })
+    .catch(error => {
+      res.status(500).json({errorMessage: "Error Creating User", error});
+    });
+
 };
 
 module.exports = {
--- api/models/userModels.js
+++ api/models/userModels.js
@@ -6,8 +6,17 @@ const SALT_ROUNDS = 11;
 
 const UserSchema = Schema({
   // create your user schema here.
-  // username: required, unique and lowercase
-  // password: required
+  username: {
+    required: true,
+    type: String,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    required: true,
+    type: String,
+  }
+
 });
 
 UserSchema.pre('save', function(next) {
@@ -15,6 +24,12 @@ UserSchema.pre('save', function(next) {
   // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
   // if there is an error here you'll need to handle it by calling next(err);
   // Once the password is encrypted, call next() so that your userController and create a user
+  bcrypt.hash(this.password, SALT_ROUNDS, (err,hash) => {
+    if (err) return next(err);
+    this.passoword = hash;
+
+    next();
+  });
 });
 
 UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
@@ -22,6 +37,11 @@ UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
   // Fill this method in with the Proper password comparing, bcrypt.compare()
   // Your controller will be responsible for sending the information here for password comparison
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
+  bcrypt.compare(plainTextPW, this.password, (err, isMatch) => {
+    if (err) return callBack(err);
+    callBack(null, isMatch);
+  });
+
 };
 
 module.exports = mongoose.model('User', UserSchema);
